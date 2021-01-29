d51e0bde237a83a58a62b666b5380525d27c0e16
Mykola Litynskyi <mklit@pop-os.localdomain>
2021-01-29 18:05:49 +0200

    commit

---
+++ zad4/build/CMakeFiles/studProgram.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/studProgram.dir/src/libtest.cpp.o"
+  "studProgram"
+  "studProgram.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/studProgram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
